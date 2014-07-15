5aa0c0843257be950d6ffeb5891fccddd304b543
Julian Adams <joolsa@gmail.com>
2014-07-15 22:19:29 +0100

    Updated for Xamarin Studio 5.0

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/Library/Application\ Support/XamarinStudio-4.0/LocalInstall/Addins
-rm ~/Library/Application\ Support/XamarinStudio-4.0/LocalInstall/Addins/java-mimetypes.dll
-cp -p bin/Debug/java-mimetypes.dll ~/Library/Application\ Support/XamarinStudio-4.0/LocalInstall/Addins/
-ls -l ~/Library/Application\ Support/XamarinStudio-4.0/LocalInstall/Addins/
+mkdir -p ~/Library/Application\ Support/XamarinStudio-5.0/LocalInstall/Addins
+rm ~/Library/Application\ Support/XamarinStudio-5.0/LocalInstall/Addins/java-mimetypes.dll
+cp -p bin/Debug/java-mimetypes.dll ~/Library/Application\ Support/XamarinStudio-5.0/LocalInstall/Addins/
+ls -l ~/Library/Application\ Support/XamarinStudio-5.0/LocalInstall/Addins/
